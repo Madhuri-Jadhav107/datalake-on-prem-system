56649a93f2a96c239b976dfc425b829cce1019d0
Madhuri-Jadhav107 <kbtug22107@kbtcoe.org>
2026-02-03 13:11:40 +0530

    Update run_ingestion.sh with final working replication flags

--- ozone-integration-lab/ozone/run_ingestion.sh
+++ ozone-integration-lab/ozone/run_ingestion.sh
@@ -2,12 +2,14 @@
 
 # Run Spark submission with all necessary dependencies for Ozone/Iceberg
 # Fixes included:
-# 1. Using ozone-filesystem-hadoop3 for Hadoop 3.x compatibility
-# 2. Including protobuf-java for missing ServiceException class
-# 3. Using local master
-# Using local ozone-filesystem.jar extracted from the server to avoid version mismatches
+# 1. Using ozone-filesystem-hadoop3-client (Shaded) to avoid Protobuf conflicts
+# 2. Setting replication to 1 (since we have 1 DataNode)
+# 3. Removing userClassPathFirst to avoid Iceberg ClassCastException
+
 docker exec madhuri-ozone-spark-iceberg-1 spark-submit \
   --master local[*] \
+  --conf spark.hadoop.ozone.replication=1 \
+  --conf spark.hadoop.ozone.replication.type=RATIS \
   --jars /home/iceberg/local/ozone-filesystem.jar \
   --packages org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.5.2 \
   /home/iceberg/local/ingest_ozone.py /home/iceberg/local/sample.csv custom_table > ingest_log.txt 2>&1
